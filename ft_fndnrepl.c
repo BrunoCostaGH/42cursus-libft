c1e02e945c483434e35b9bd2d9a2765af09ef73d
Bruno Miguel Silva Costa <bsilva-c@c1r1s4.42porto.com>
2023-04-28 21:14:33 +0100

    fix(ft_fndnrepl.c): fix incorrect if statement

--- ft_fndnrepl.c
+++ ft_fndnrepl.c
@@ -6,7 +6,7 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 16:53:27 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/04/28 16:53:55 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/04/28 20:56:57 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ char	*ft_fndnrepl(const char *find_str, char *replace_word, \
 	while (temp_str && temp_str++ != str)
 		str_pos++;
 	ft_strlcpy(before_str, find_str, str_pos);
-	if (ft_strlen(temp_str) > ft_strlen(replace_word))
+	if (ft_strlen(temp_str) >= ft_strlen(replace_word))
 		after_str = temp_str + (ft_strlen(replace_word) - 1);
 	temp_str = ft_strjoin(before_str, replace_value);
 	if (after_str)
