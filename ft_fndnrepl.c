32cbdf603f3d526d2966e518a627be66f31e9209
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-05-01 19:18:23 +0100

    fix(ft_fndnrepl.c): fix memory read/write errors

--- ft_fndnrepl.c
+++ ft_fndnrepl.c
@@ -6,7 +6,7 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 16:53:27 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/04/28 20:56:57 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/05/01 19:17:08 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_fndnrepl(const char *find_str, char *replace_word, \
 	str_pos = 1;
 	after_str = NULL;
 	before_str = ft_calloc(ft_strlen(find_str) - ft_strlen(replace_word) + \
-							ft_strlen(replace_value), sizeof(char));
+							ft_strlen(replace_value) + 1, sizeof(char));
 	str = ft_strnstr(find_str, replace_word, ft_strlen(find_str));
 	temp_str = (char *)find_str;
 	while (temp_str && temp_str++ != str)
