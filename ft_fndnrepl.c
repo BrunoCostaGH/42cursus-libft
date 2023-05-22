40535fe643302f9027570c858b39d8d0b923bc81
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-05-22 15:12:27 +0100

    fix(ft_fndnrepl.c): fix unprotected malloc

--- ft_fndnrepl.c
+++ ft_fndnrepl.c
@@ -6,30 +6,36 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 16:53:27 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/05/01 19:17:08 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/05/22 15:09:20 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	len_before_replace(char *find_str, char *replace_word)
+{
+	int	i;
+
+	while (find_str && find_str++ != replace_word)
+		i++;
+	return (i);
+}
+
 char	*ft_fndnrepl(const char *find_str, char *replace_word, \
 							char *replace_value)
 {
-	int		str_pos;
 	char	*before_str;
 	char	*str;
 	char	*after_str;
 	char	*temp_str;
 
-	str_pos = 1;
-	after_str = NULL;
 	before_str = ft_calloc(ft_strlen(find_str) - ft_strlen(replace_word) + \
 							ft_strlen(replace_value) + 1, sizeof(char));
+	if (!before_str)
+		return (0);
 	str = ft_strnstr(find_str, replace_word, ft_strlen(find_str));
 	temp_str = (char *)find_str;
-	while (temp_str && temp_str++ != str)
-		str_pos++;
-	ft_strlcpy(before_str, find_str, str_pos);
+	ft_strlcpy(before_str, find_str, len_before_replace(temp_str, str));
 	if (ft_strlen(temp_str) >= ft_strlen(replace_word))
 		after_str = temp_str + (ft_strlen(replace_word) - 1);
 	temp_str = ft_strjoin(before_str, replace_value);
