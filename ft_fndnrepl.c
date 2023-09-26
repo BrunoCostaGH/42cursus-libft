3296373bab17c5134e50afd536afd43b75d2c662
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-09-26 19:54:16 +0100

    fix(ft_fndnrepl.c): fix segmentation fault condition on null find_str

--- ft_fndnrepl.c
+++ ft_fndnrepl.c
@@ -6,13 +6,13 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 16:53:27 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/09/25 19:06:18 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/09/26 16:35:15 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	len_before_replace(char *find_str, const char *replace_word)
+static int	len_before_replace(const char *find_str, const char *replace_word)
 {
 	int	i;
 
@@ -31,16 +31,16 @@ char	*ft_fndnrepl(const char *find_str, char *replace_word, \
 	char	*after_str;
 	char	*temp_str;
 
+	if (!find_str || !replace_word || !replace_value)
+		return (0);
 	before_str = ft_calloc(ft_strlen(find_str) - ft_strlen(replace_word) + \
 							ft_strlen(replace_value) + 1, sizeof(char));
 	if (!before_str)
 		return (0);
-	str = NULL;
 	after_str = NULL;
-	temp_str = (char *)find_str;
 	ft_strlcpy(before_str, find_str, \
-	len_before_replace(temp_str, replace_word));
-	temp_str += len_before_replace(temp_str, replace_word);
+	len_before_replace(find_str, replace_word));
+	temp_str = (char *)find_str + len_before_replace(find_str, replace_word);
 	if (ft_strlen(temp_str) >= ft_strlen(replace_word))
 		after_str = temp_str + (ft_strlen(replace_word) - 1);
 	temp_str = ft_strjoin(before_str, replace_value);
