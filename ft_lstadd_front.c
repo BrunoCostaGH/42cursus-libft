2982c11099e63dd9cafa9c48be8ff17de5280893
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-10-02 15:08:09 +0100

    feat(ft_lstadd_front_c): add new as list head if head doesn't exist

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 15:42:29 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/03/15 18:22:05 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/10/02 15:07:28 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (lst && new)
+	if (!*lst)
+		*lst = new;
+	else
 	{
 		new->next = *lst;
 		*lst = new;
